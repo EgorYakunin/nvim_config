4203453aecc0f38c4eebd70e3dafa86c1ea44948
EgorYakunin <e.yakunin04@gmail.com>
2023-04-28 17:56:47 +0300

    init commit

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,8 @@
+-- Set tab width to 4 spaces
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+
+-- Use tabs instead of spaces
+vim.opt.expandtab = false
+vim.opt.relativenumber = true
